1caa18894ccf5b5745545188fb630e1f46852d59
Faridah Namutebi <mugen64@users.noreply.github.com>
2022-02-22 17:09:04 +0300

    mor ch

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calendar',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+        accentColor: Colors.white,
+      ),
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
